4bd1961e651c6596001139d8b9aad617b3ba9691
Crystal <kiettanx20@gmailk.com>
2020-12-04 22:03:52 +0700

    before check1

--- app/view/quanli/thongtinxe.php
+++ app/view/quanli/thongtinxe.php
@@ -1,5 +1,7 @@
 <?php
 require_once ('app/controller/quanli_controller.php');
+require_once ('app/controller/banhang_controller.php');
+$banhang_controller = new banhang_controller();
 if (!isset($_SESSION['username'])){
     redirect('index.php?controller=login&action=logout');
 }
@@ -13,8 +15,25 @@ if (isset($_POST['id_delete'])){
     quanli_controller::set_xe_state_by_id($_POST['id_delete']);
 }
 $result = quanli_controller::get_all_xe();
-if (isset($_POST['search_value'])) {
-    $result = quanli_controller::search_xe_by_tenxe($_POST['search_value']);
+if (isset($_POST['search_value']) && isset($_POST['select_value'])) {
+    $search_value = $_POST['search_value'];
+    $select_value = $_POST['select_value'];
+    if (empty($search_value)) {
+        $result = $banhang_controller->get_all_cars();
+    }
+    else {
+        switch ($select_value) {
+            case "Mã xe":
+                $result = $banhang_controller->get_car_by_maxe($search_value);
+                break;
+            case "Tên xe":
+                $result = $banhang_controller->search_tenxe_xe_model($search_value);
+                break;
+            case "Mã nhà cung cấp":
+                $result = $banhang_controller->search_maNCC($search_value);
+                break;
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -57,16 +76,33 @@ if (isset($_POST['search_value'])) {
 
 <div class="container">
     <div class="row">
-        <div >
-            <h3 class="text-center mb-3" >Tìm xe</h3>
-            <form action="#" method="POST">
-                <input type="text" name="search_value" class="form-control" placeholder="Nhập tên xe">
-                <input type="submit" class="form-control" value="Tìm">
-            </form>
+        <h4 class="text-center">Tìm kiếm thông tin xe</h4>
+        <form method="post">
+            <table style="margin: auto auto">
 
-        </div>
+                <tr>
+                    <td style="width: 100px">
+                        <select class="form-control" name="select_value">
+                            <option value="Mã xe">Theo Mã xe</option>
+                            <option value="Tên xe">Theo Tên xe</option>
+                            <option value="Mã nhà cung cấp">Theo Mã nhà cung cấp</option>
+                        </select>
+                    </td>
+                    <td style="width: 150px">
+                        <input type="text" name="search_value" class="form-control" placeholder="Nhập thông tin tìm kiếm">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2"><input type="submit" class="btn btn-danger px-5 mr-2 form-control" value="Tìm kiếm"></td>
+                </tr>
+
+            </table>
+        </form>
+
     </div>
-</div>
+</div> <br><br>
+
 
 <div id="responsive_table">
     <table id="table_list">
